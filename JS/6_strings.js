f6a6d06be3246e45ba1751ae4fefae2f45345d32
harshadatapre <tapre.harshada13@gmail.com>
2026-04-27 16:31:03 +0530

    learned string

---
+++ JS/6_strings.js
@@ -0,0 +1,73 @@
+let a = "Harshada"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+console.log(a[7])
+console.log(a[8])  //js is also called as a forgiving language
+
+console.log(a.length)
+
+let name = "Harshada"
+let friend = "Rohan"
+console.log("His name is " + name + " and his friend's name is " + friend)
+console.log(`His name is ${name} and his frined's name is ${friend}`) 
+
+// go on any page
+// right click
+// select inspect
+// go to console
+// here repl chal rahi hoti h 
+// u can write js there
+
+let e = "Roh'an"
+console.log(e)
+
+let c = "Roh\"an"
+console.log(c)
+
+let d = "Roh\nan"
+console.log(d)
+
+let b = "ShivamSh"
+console.log(b.toUpperCase()) 
+console.log(b.toLowerCase()) 
+console.log(b.length)
+console.log(b.slice(1, 5)) 
+console.log(b.slice(1))
+console.log(b.replace("Sh", "77"))  //only first occurace will replace
+console.log(b.concat(a, "Aishwarya", "Rahul", "Priya"))
+
+/* if u wat to explore more then go to the console
+   write b = "Rohan" then enter then write b. and u will see 
+   so many functions ...explore them*/
+
+// practice set 
+
+// 1
+console.log("har\"".length)
+
+//2
+let f = "Reeya"
+
+console.log(f.includes('o', 5))
+console.log(f.includes("Ree", 0))
+console.log(f.includes("Reeya"))
+
+console.log(f.startsWith("Ree"))
+console.log(f.startsWith("ey")) 
+
+console.log(f.endsWith("Ree"))
+console.log(f.endsWith("ya"))
+
+// 3
+let str = "The SECRET Of Getting Ahead Is Getting STARTED"
+console.log(str.toUpperCase())
+
+// 4
+let s = "Please give Rs 1000"
+console.log()
+
