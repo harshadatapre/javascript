3c4c8ea79137b37f97af7d6344decf6132c38369
harshadatapre <tapre.harshada13@gmail.com>
2026-07-07 17:22:32 +0530

    learned

--- JS/10_practice_set.js
+++ JS/10_practice_set.js
@@ -1,4 +1,28 @@
-let inp = prompt("Enter your age")
+// 1, 2, 3
+// let restart
 
-if(inp < 18) alert("You cannot drive.")
-else alert("You can drive")
+// do {
+//     let age = prompt("Enter your age")
+
+//     if(age < 0) console.error("Age is invalid")
+//     else if(age < 18) alert("You cannot drive.")
+//     else alert("You can drive")
+
+//     restart = confirm("Do you want to see prompt again")
+
+// } while(restart);
+
+//______________________________________________________________________
+
+// 4
+// let n = prompt("Enter a number")
+
+// if(n > 4) location.href = "https://google.com" 
+
+// ___________________________________________________________
+
+
+//5
+let color = prompt("Enter a color")
+document.body.style.background = color
+
