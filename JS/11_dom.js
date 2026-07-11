0746a276c67690d80a08d46f3277927afcf61aff
harshadatapre <tapre.harshada13@gmail.com>
2026-07-11 17:56:01 +0530

    learned dom

---
+++ JS/11_dom.js
@@ -0,0 +1,5 @@
+console.log("Hello World")
+
+document.body.firstElementChild              // first element
+document.body.firstElementChild.childNodes  //all child nodes
+document.body.firstElementChild.children    //only html elements
