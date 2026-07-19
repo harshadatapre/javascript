635599cb011ded4f4a3b9692cf0a2939d9b6e4c2
harshadatapre <tapre.harshada13@gmail.com>
2026-07-19 20:52:37 +0530

    updated dom

--- JS/11_dom.js
+++ JS/11_dom.js
@@ -1,5 +1,19 @@
 console.log("Hello World")
 
-document.body.firstElementChild              // first element
+console.log(document.body)
+let cont = document.body.childNodes[1]
+console.log(cont.firstChild)
+console.log(cont.lastChild)
+console.log(cont.firstElementChild)
+console.log(cont.lastElementChild)
+console.log(cont.lastElementChild.style.color = 'red')
+console.log(cont.lastElementChild.style.background = 'pink')
+console.log(cont.lastElementChild.parentElement)
+console.log(document.body.firstElementChild.childNodes)
+console.log(document.body.firstElementChild)              // first element
 document.body.firstElementChild.childNodes  //all child nodes
-document.body.firstElementChild.children    //only html elements
+console.log(document.body.firstElementChild.children)    //only html elements
+console.log(document.body.firstElementChild.children[1])
+console.log(document.body.firstElementChild.children[1].nextElementSibling)
+console.log(document.body.firstElementChild.children[1].previousElementSibling)
+console.log(document.body.firstElementChild.children[1].previousSibling)
